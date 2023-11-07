9262a3535ab1e3d5555d59f0e302ec457537a735
Muhammed Sobuj <sobujhosen.tisi@gmail.com>
2023-11-07 21:51:50 +0600

    Baraz Website

---
+++ js/custom.js
@@ -0,0 +1,93 @@
+(function() {
+	'use strict';
+
+	var tinyslider = function() {
+		var el = document.querySelectorAll('.testimonial-slider');
+
+		if (el.length > 0) {
+			var slider = tns({
+				container: '.testimonial-slider',
+				items: 1,
+				axis: "horizontal",
+				controlsContainer: "#testimonial-nav",
+				swipeAngle: false,
+				speed: 700,
+				nav: true,
+				controls: true,
+				autoplay: true,
+				autoplayHoverPause: true,
+				autoplayTimeout: 3500,
+				autoplayButtonOutput: false
+			});
+		}
+	};
+	tinyslider();
+
+	
+
+
+	var sitePlusMinus = function() {
+
+		var value,
+    		quantity = document.getElementsByClassName('quantity-container');
+
+		function createBindings(quantityContainer) {
+	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
+	      var increase = quantityContainer.getElementsByClassName('increase')[0];
+	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
+	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
+	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
+	    }
+
+	    function init() {
+	        for (var i = 0; i < quantity.length; i++ ) {
+						createBindings(quantity[i]);
+	        }
+	    };
+
+	    function increaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        console.log(quantityAmount, quantityAmount.value);
+
+	        value = isNaN(value) ? 0 : value;
+	        value++;
+	        quantityAmount.value = value;
+	    }
+
+	    function decreaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        value = isNaN(value) ? 0 : value;
+	        if (value > 0) value--;
+
+	        quantityAmount.value = value;
+	    }
+	    
+	    init();
+		
+	};
+	sitePlusMinus();
+
+
+})()
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
